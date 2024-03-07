424c3919a81d11c02bd90ba8d2736918f6ae5bc4
vlad05x <organizm777alf@gmail.com>
2024-03-07 20:34:55 +0200

    Performed the construction of functions and added prototype

--- lab08/prj/ModulesHoncharenko.h
+++ lab08/prj/ModulesHoncharenko.h
@@ -4,4 +4,10 @@
 
 double s_calculation(double x, double y, double z);
 
+void calcWave(double grade, double& waveHeight, std::string& seaDescription);
+
+void countNumbers(int n, int& negativeCount, int& rangeCount);
+
+void calcBinaryInfo(unsigned int N, bool& D1, int& binaryCount)
+
 #endif // MODULES_SURNAME_H
--- lab09/prj/ModulesHoncharenko/ModulesHoncharenko.cpp
+++
@@ -1,6 +0,0 @@
-// ModulesПрізвище.cpp (Реалізація функції s_calculation)
-#include <cmath>
-
-double s_calculation(double x, double y, double z) {
-    return z * sin(pow(x, 2) * y) + sqrt(fabs(z - 12 * x)) / pow(y, 3);
-}
