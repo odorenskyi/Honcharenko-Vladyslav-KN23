b6ec4f16921b4010fe84dd2dda20ee6ccf8a43b7
vlad05x <organizm777alf@gmail.com>
2024-03-25 20:55:15 +0200

    Implemented functions for tasks 10.1-10.3

--- lab08/prj/ModulesHoncharenko.h
+++ lab08/prj/ModulesHoncharenko.h
@@ -10,4 +10,18 @@ void countNumbers(int n, int& negativeCount, int& zeroCount, int& rangeCount);
 
 void calcBinaryInfo(unsigned int N, bool& D1, int& binaryCount);
 
+int countVowels(string word);
+
+bool checkWordInVerse(string word);
+
+void processFiles(const string& input, const string& output);
+
+int countConsonants(string word);
+
+void writeToInputFile(const string& input);
+
+double calcS_calculation (double x, double y, double z);
+
+string BinaryNum(int n);
+
 #endif // MODULES_HONCHARENKO_H
--- lab08/prj/ModulesHoncharenko/ModulesHoncharenko.cpp
+++ lab08/prj/ModulesHoncharenko/ModulesHoncharenko.cpp
@@ -1,6 +1,8 @@
 #include <cmath>
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <ctime>
 
 using namespace std;
 
@@ -8,6 +10,7 @@ double s_calculation(double x, double y, double z) {
     return z * sin(pow(x, 2) * y) + sqrt(fabs(z - 12 * x)) / pow(y, 3);
 }
 
+// Function for Lab 9
 void calcWave(double grade, double& waveHeight, std::string& seaDescription) {
     if (grade < 0 || grade > 9) {
         waveHeight = 0;
@@ -78,4 +81,115 @@ void calcBinaryInfo(unsigned int N, bool& D1, int& binaryCount) {
         }
         N /= 2;
     }
+}
+
+
+// Function for Lab 10.1
+// Function for counting the number of vowels in a word
+int countVowels(string word) {
+    int count = 0;
+    string vowels = "aeiouаеіїоуюя";
+    for (char& c : word) {
+        if (vowels.find(c) != string::npos) {
+            count++;
+        }
+    }
+    return count;
+}
+
+// Function to check the presence of a word in the specified string
+bool containsWord(const string& poem, const string& word) {
+    return poem.find(word) != string::npos;
+}
+
+// Basic function for processing files
+void processFiles(const string& input, const string& output) {
+    ifstream inputFile(input);
+    if (!inputFile.is_open()) {
+        cout << "Неможливо відкрити файл!";
+        return;
+    }
+
+    string inputText;
+    getline(inputFile, inputText);
+
+    inputFile.close();
+
+    ofstream outputFile(output);
+    if (!outputFile.is_open()) {
+        cout << "Неможливо відкрити файл!";
+        return;
+    }
+
+    outputFile << "Авторська інформація: \nІм'я та прізвище розробника модуля: Гончаренко Влад, \nУстанова/організація: ЦНТУ, \nМісто: Кропівницький, \nКраїна: Україна, \nРік розробки: 2024\n";
+
+    int vowelCount = countVowels(inputText);
+    outputFile << "Кількість голосних літер у слові з вхідного файла: " << vowelCount;
+
+    string poem = "До щастя не пускає лінощів орава.\nУ чім воно - ніхто не знає до пуття.\nНавчитись радісно робити кожну справу -\nНайперше правило щасливого життя.";
+    bool containsWordInPoem = containsWord(poem, inputText);
+    outputFile << "Слово з вихідного файлу " << (containsWordInPoem ? "є" : "не є") << " у вірші Віталя Іващенка\n";
+
+    outputFile.close();
+
+    cout << "Обробку завершено. Висновок записаний у " << output;
+}
+
+//Task 10.2
+int countConsonants(string word) {
+    int countCons = 0;
+    string consonants = "bcdfghjklmnpqrstvwxyzбвгґджзйклмнпрстфхцчшщюя";
+    for (char& ch : word) {
+        if (consonants.find(ch) != string::npos) {
+            countCons++;
+        }
+    }
+    return countCons;
+}
+// Function to write the number of consonant letters, date and time to the output file
+void writeToInputFile(const string& input) {
+    ofstream inputFile(input, ios_base::app);
+    if (!inputFile.is_open()) {
+        cout << "Неможливо відкрити файл!";
+        return;
+    }
+// Time and date
+    time_t currentTime = time(nullptr);
+    tm* localTime = localtime(&currentTime);
+    char timeBuffer[50];
+    strftime(timeBuffer, 50, "%Y-%m-%d %H:%M:%S", localTime);
+
+    ifstream inputFileRead(input);
+    if (!inputFileRead.is_open()) {
+        cerr << "Unable to open input file for reading" << endl;
+        return;
+    }
+
+    string fileContent((istreambuf_iterator<char>(inputFileRead)), istreambuf_iterator<char>());
+    inputFileRead.close();
+
+    int consonantCount = countConsonants(fileContent);
+
+    inputFile << "Кількість приголосних літер у тексті: " << consonantCount;
+    inputFile << "Дата та час дозапису: " << timeBuffer;
+
+    inputFile.close();
+
+    cout << "Дані успішно записано у вхідний файл!";
+}
+
+//Task 10.3
+double calcS_calculation (double x, double y, double z) {
+    return s_calculation(x, y, z);
+}
+
+string BinaryNum(int n) {
+    if (n == 0)
+        return "0";
+    string binary = "";
+    while (n > 0){
+        binary = to_string(n % 2) + binary;
+        n = n / 2;
+    }
+    return binary;
 }
